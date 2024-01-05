887b81f76ea2e7adbaf4beef79c264e7adb53d7c
baddesign123 <baddesignhaiti@gmail.com>
2024-01-05 12:23:30 -0500

    lot

--- final.py
+++ final.py
@@ -10,5 +10,47 @@ class GestionSondage:
         self.file_name_s3 = 'reponses_sondage.json'
 
     
+    def repondre_sondage(self):
+        q1 = "Intentions de quitter le pays"
+        q2 = "Tranche dage"
+        q3 = "Niveau detudes"
+        q4 = "Pays vise"
+        q5 = "Raison du depart"
+        q6 = "Objectif du depart"
+        q7 = "Duree prevue à letranger"
+        q8 = "Intention de retour dans le pays dorigine"
+
+        r1 = ["Oui", "Non", "Incertain"]
+        r2 = ["Moins de 20 ans", "20-24 ans", "25-29 ans", "30-34 ans", "35 ans et plus"]
+        r3 = ["Licence 1", "Licence 2", "Licence 3", "Licence 4", "DUT 1", "DUT 2"]
+        r4 = ["Etats-Unis", "Canada", "Royaume-Uni", "Australie", "France", "Autre"]
+        r5 = ["Opportunites professionnelles", "Recherche academique", "Qualite de vie", "Autre"]
+        r6 = ["Etudes supplementaires", "Raisons professionnelles", "Raisons personnelles"]
+        r7 = ["Moins d'un an", "1-2 ans", "3-5 ans", "Plus de 5 ans"]
+        r8 = ["Oui", "Non", "Incertain"]
+
+        questions = [q1, q2, q3, q4, q5, q6, q7, q8]
+        reponses = [r1, r2, r3, r4, r5, r6, r7, r8]
+
+        reponse_etudiant = {}
+        nom = input("Entrer votre nom: ")
+        reponse_etudiant = {"Nom": nom}
+        for i in range(len(questions)):
+            print(f"Question {i + 1}: {questions[i]}")
+            if isinstance(reponses[i], list):
+                for j in range(len(reponses[i])):
+                    print(f"    {j + 1}. {reponses[i][j]}")
+            else:
+                print(f"    {reponses[i]}")
+
+            reponse = input(f"Choisissez la lettre de votre reponse: ")
+            reponse = int(reponse)
+
+            while reponse > len(reponses[i]):
+                reponse = input("La reponse choisie n'est pas convenable: ")
+                reponse = int(reponse)
+
+            r = reponses[i][reponse - 1]
+            reponse_etudiant[questions[i]] = r
 
 
